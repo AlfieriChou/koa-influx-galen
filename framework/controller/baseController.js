a66abdb22ff9aa0e9b601eeefeb04fca84e5b295
AlfieriChou <alfierichou@gmail.com>
2020-08-16 09:36:33 +0800

    chore: update baseContriller

--- framework/controller/baseController.js
+++ framework/controller/baseController.js
@@ -1,13 +1,22 @@
 const _ = require('lodash')
 
-const parseFilter = (tableName, filter) => {
+const parseFilter = ({
+  tableName, filter, properties, tags
+}) => {
   const {
     where, order, limit, offset
   } = filter
   let query = `select * from ${tableName}`
   if (Object.keys(where).length > 0) {
     Object.entries(where).forEach(([key, value]) => {
-      query += ` where ${key} = '${value}'`
+      if (!properties[key] || !properties[key].type || !tags.includes(key)) {
+        throw new Error(`${key} is not a valid property`)
+      }
+      if (properties[key].type === 'string') {
+        query += ` where ${key} = '${value}'`
+      } else {
+        query += ` where ${key} = ${value}`
+      }
     })
   }
   if (order) {
@@ -24,21 +33,26 @@ const parseFilter = (tableName, filter) => {
 
 module.exports = class Controller {
   static async index (ctx) {
-    const filter = ctx.query.filter ? JSON.parse(ctx.query.filter) : {
-      where: {}, order: 'time desc', limit: 100, offset: 0
-    }
-    return ctx.influx.query(parseFilter(ctx.tableName, filter))
+    const { query: { filter }, schema, tableName } = ctx
+    return ctx.influx.query(parseFilter({
+      tableName,
+      filter: filter ? JSON.parse(filter) : {
+        where: {}, order: 'time desc', limit: 100, offset: 0
+      },
+      ...schema
+    }))
   }
 
   // eslint-disable-next-line consistent-return
   static async create (ctx) {
     const { request: { body }, schema, tableName } = ctx
     try {
-      return ctx.influx.writePoints([{
+      await ctx.influx.writePoints([{
         measurement: tableName,
         tags: _.pick(body, schema.tags || []),
         fields: _.omit(body, schema.tags || [])
       }])
+      return { success: true }
     } catch (err) {
       ctx.throw(400, '数据写入失败')
     }
