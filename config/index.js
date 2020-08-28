c5e8c837e69c2293e3a64e28e3d7baa7aad8a4cb
AlfieriChou <alfierichou@gmail.com>
2020-08-28 17:51:21 +0800

    feat: add config

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  influx: {
+    host: '127.0.0.1',
+    database: 'test'
+  }
+}
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const initializeApp = require('./framework/initializeApp')
+const config = require('./config')
 
 const bootstrap = async () => {
-  const app = await initializeApp({})
+  const app = await initializeApp(config)
   app.listen(3000, () => {
     console.log('Listening on port 3000')
   })
